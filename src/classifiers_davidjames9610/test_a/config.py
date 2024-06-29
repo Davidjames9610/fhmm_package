efce23257f59cef87effc9e22aef8247d4867cf2
David Edwards <davidjames9610@gmail.com>
2024-06-29 17:16:59 -0500

    real noise started and speaker A database working

--- src/classifiers_davidjames9610/test_a/config.py
+++ src/classifiers_davidjames9610/test_a/config.py
@@ -1,8 +1,14 @@
 
 
-basedirs = {
-    'ivector' : 'src.classifiers_davidjames9610.test_a.ivector_speech_data.config',
-    'whale': 'src.classifiers_davidjames9610.test_a.whale_data.config'
-}
+basedirs = [
+    'src.classifiers_davidjames9610.test_a.speech_commands.config',
+    'src.classifiers_davidjames9610.test_a.ivector_speech_data.config',
+    'src.classifiers_davidjames9610.test_a.whale_data.config'
+]
 
-config_location = basedirs['whale']
+config_location = basedirs[0]
+
+# MUSAN noise
+# sound keys
+UNDER_WATER_DRONE = '/Users/david/Documents/data/MUSAN/musan/noise/free-sound/noise-free-sound-0000.wav'
+PEOPLE_TALKING = '/Users/david/Documents/data/MUSAN/musan/noise/free-sound/noise-free-sound-0000.wav'
--- src/classifiers_davidjames9610/test_a/speech_commands/config.py
+++ src/classifiers_davidjames9610/test_a/speech_commands/config.py
@@ -1,8 +1,12 @@
-# define constants for test
 
-sr = 16000  # 22050
+# define constants for test
+sr = 16000
+
+# file locations
+basedir = '/Users/david/Documents/code/fhmm/v1/src/classifiers_davidjames9610/test_a/speech_commands'
+samples_labels = basedir + '/samples_labels.pickle'
+classifiers = basedir + '/classifiers.pickle'
+features = basedir + '/features.pickle'
 
 # noise types
-GAUSS = 'gauss'
-
-dataset_loc = r"/Users/david/Documents/data/speechCommands"
+# GAUSS = 'gauss'
